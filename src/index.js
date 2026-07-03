e412289f3afc2ecfa8145897c7347002e71b967f
Axcyzp <191786399+Axcyzp@users.noreply.github.com>
2026-07-03 19:00:22 +0500

    Fixed compatibility issues with older devices

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "react-app-polyfill/stable";
+import "whatwg-fetch";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -8,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 reportWebVitals();
